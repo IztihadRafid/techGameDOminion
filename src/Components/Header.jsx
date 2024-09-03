216e0584215da3f6d953b41765965ded876f47cc
IztihadRafid <iztihadrafid10@gmail.com>
2024-09-03 22:49:25 +0600

    text style added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import BreakingNews from './BreakingNews.jsx/BreakingNews';
-
+ import "./style.css"
 const Header = () => {
     return (
         <div className='my-4'>
-            <h1 className='font-doppio text-4xl font-bold text-center'>Ultimate Gaming Arena 2024</h1>
+            <h1 className='animated-header font-doppio text-6xl font-bold text-center'>Ultimate Gaming Arena 2024</h1>
             <BreakingNews></BreakingNews>
         </div>
     );
